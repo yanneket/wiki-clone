8604b0907c9a02170960a19cd668f02e802bdb5b
yanneket <marabus@mail.ru>
2025-06-14 11:53:44 +0300

    calc2

--- middle_site/static/sw.js
+++ middle_site/static/sw.js
@@ -1,13 +1,2 @@
-self.addEventListener("install", (e) => {
-  e.waitUntil(
-    caches.open("pwa-cache").then((cache) =>
-      cache.addAll(["/calculator", "/static/calculator/index.html"])
-    )
-  );
-});
-
-self.addEventListener("fetch", (e) => {
-  e.respondWith(
-    caches.match(e.request).then((r) => r || fetch(e.request))
-  );
-});
+self.addEventListener('install', e => e.waitUntil(self.skipWaiting()));
+self.addEventListener('activate', e => e.waitUntil(self.clients.claim()));
